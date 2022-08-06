f3cc48ac9d94d07b19b7e7af4d8b2063dff52524
Mahadev1 <mahadev.hallikhed130@gmail.com>
2022-08-06 22:14:48 +0530

    first commit

---
+++ WorkingProgram/src/com/test/Test.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// create a list of strings
+        List<String> names = Arrays.asList(
+            "Geek", "GeeksQuiz", "g1", "QA", "Geek2");
+  
+        // declare the predicate type as string and use
+        // lambda expression to create object
+        Predicate<String> p = (s) -> s.startsWith("G");
+  
+        // Iterate through the list
+        for (String st : names) {
+            // call the test method
+            if (p.test(st))
+                System.out.println(st);
+        }
+
+	}
+
+}
